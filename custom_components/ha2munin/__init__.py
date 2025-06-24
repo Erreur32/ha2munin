cbac921a3923f64ad4f4b27a932f53a4fb6a045c
Erreur32 <erreur32@echosystem.fr>
2025-06-24 16:00:07 +0200

    Fix

--- custom_components/ha2munin/__init__.py
+++ custom_components/ha2munin/__init__.py
@@ -3,3 +3,7 @@ from .http import async_setup as setup_view
 async def async_setup(hass, config):
     await setup_view(hass, config)
     return True
+
+async def async_setup_entry(hass, entry):
+    await setup_view(hass, {})
+    return True
